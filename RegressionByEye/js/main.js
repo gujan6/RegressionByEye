57d2b723f399b52487186a321d323ee8fda1e38a
Jan Gugler <jan.gugler@gmail.com>
2019-04-07 17:32:13 +0200

    Different chart types

--- RegressionByEye/js/main.js
+++ RegressionByEye/js/main.js
@@ -1,5 +1,9 @@
 //There are twenty images per graph folder
 
+//Global variables
+var globalSequence = 0;
+var maxSequenceLength = 10;
+
 //This function returns an array with a sequence of ten numbers
 function getTenImages(){
   var imageNrArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -12,23 +16,30 @@ function getTenImages(){
   return imageNrArray
 }
 
-//Changes the image based on the slider position
-function changeImage(){
+//Shows images in the passed folder and changes the image based on the slider position
+function changeImage(folder){
   let numberArray = getTenImages();
   var defaultImageNumber = numberArray[0];
   var slider = document.getElementById("myRange");
   var output = document.getElementById("sliderValue");
   output.innerHTML = slider.value;
   //Get first image, based on the first number in the array
-  document.getElementById("img").src = "img/" + defaultImageNumber + ".png";
+  document.getElementById("img").src = folder + defaultImageNumber + ".png";
   slider.oninput = function() {
     output.innerHTML = this.value;
-    document.getElementById("img").src = "img/"+ numberArray[this.value - 1] + ".png";
+    document.getElementById("img").src = folder + numberArray[this.value - 1] + ".png";
   }
 }
 
 function submitAnswer(){
   //Writes answer to an array, which can then be converted to csv, and triggers the next image
+  if(globalSequence + 1 === maxSequenceLength){
+    //Create CSV
+  }
+  else {
+    globalSequence++; //increments the global sequence by 1, so that the next question is displayed.
+    startExperiment(globalSequence);
+  }
 }
 
 //Returns the selected participant from the dropdown
@@ -49,10 +60,19 @@ function getParticipant(){
 
 function getExperimentSequence(participantNumber){
   //Code for the sequence
+  //Should pick make a list of filepaths which contain the images
+  let folderArray;
+  switch (participantNumber){
+    case 1:
+      return folderArray = ["img/", "img/scatter/XXXX"];
+    case 2:
+      return folderArray = ["img/", "img/scatter/XXXX"];
+  }
+
 }
 
-function startExperiment(){
+function startExperiment(sequenceStep){
   var participantNumber = getParticipant();
-  getExperimentSequence(participantNumber);
-  changeImage();
+  var folderArray = getExperimentSequence(participantNumber);
+  changeImage(folderArray[sequenceStep]);
 }
